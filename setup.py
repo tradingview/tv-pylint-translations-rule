e7befcee5adc382868c17570ab3f0192f1ffe3d2
Igor Emets <iemets@mail.tradingview.com>
2017-05-24 13:30:33 +0300

    Exclude some chars from translation-of-non-latin-string

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='pylint-translations-rule',
 
-    version='0.0.1',
+    version='0.0.2',
 
     url='https://github.com/pypa/sampleproject',
 
@@ -18,5 +18,10 @@ setup(
 
     packages=find_packages(),
 
-    install_requires=['pylint', 'astroid', 'pytest', 'six'],
+    install_requires=[
+        'pylint==1.6.5',
+        'astroid==1.4.9',
+        'pytest==3.0.7',
+        'six'
+    ],
 )

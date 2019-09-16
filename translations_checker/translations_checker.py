e70b11f576685ddff447690809f46e5ca8f012d9
salyarka <salyarka@gmail.com>
2019-09-16 14:02:27 +0300

    Deleted excess check of py2-str/py3-bytes.

--- translations_checker/translations_checker.py
+++ translations_checker/translations_checker.py
@@ -74,7 +74,6 @@ class TranslationsChecker(BaseChecker):
 	])
 
 	NON_LATIN_CHARS_RE = re.compile(r"([^a-z^A-Z^\d^\s^<^>^/^\\^\|^\:^\;^\.^\,^\~^\’^\“^\”^\-^\–^\—^\_^\'^\`^\"^\$^!^?^@^#^%^&^*^(^)^\[^\]^\{^\}^=^+]+)", re.UNICODE | re.MULTILINE)
-	UNICODE_NON_LATIN_CHARS_RE = re.compile(ur"([^a-z^A-Z^\d^\s^<^>^/^\\^\|^\:^\;^\.^\,^\~^\’^\“^\”^\-^\–^\—^\_^\'^\`^\"^\$^!^?^@^#^%^&^*^(^)^\[^\]^\{^\}^=^+]+)", re.UNICODE | re.MULTILINE)
 	HTML_TAGS_RE = re.compile(r"<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>")
 
 
@@ -105,9 +104,7 @@ class TranslationsChecker(BaseChecker):
 		if isinstance(node, astroid.Const) and isinstance(node.value, six.string_types):
 			if self.HTML_TAGS_RE.search(node.value):
 				self.add_message(self.ERROR_MESSAGES_ID['html-string'], node=self.current_func_node)
-			if isinstance(node.value, str) and self.NON_LATIN_CHARS_RE.search(node.value):
-				self.add_message(self.ERROR_MESSAGES_ID['non-latin-string'], node=self.current_func_node)
-			if isinstance(node.value, unicode) and self.UNICODE_NON_LATIN_CHARS_RE.search(node.value):
+			if self.NON_LATIN_CHARS_RE.search(node.value):
 				self.add_message(self.ERROR_MESSAGES_ID['non-latin-string'], node=self.current_func_node)
 			return
 
